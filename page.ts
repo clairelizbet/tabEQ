e5d115a744e602a135512948a75619c51777ec64
claire <claire@clairecampbell.io>
2022-05-15 02:56:10 -0500

    Move event listener out of prototype fn

--- page.ts
+++ page.ts
@@ -6,16 +6,14 @@ AudioContext.prototype._createMediaElementSource =
 AudioContext.prototype.createMediaElementSource = function (
   mediaElement: HTMLMediaElement
 ) {
-  const audioCtx = this
-  const source = audioCtx._createMediaElementSource(mediaElement)
-
-  window.addEventListener('message', (event) => {
-    if (event.source != window) return
-    if (event.data?.type === 'audioNodesUpdate' && event.data?.settings) {
-      updateSettings(event.data?.settings)
-    }
-  })
-
+  const source = this._createMediaElementSource(mediaElement)
   connectAVSourceNode(source)
   return source
 }
+
+window.addEventListener('message', (event) => {
+  if (event.source != window) return
+  if (event.data?.type === 'audioNodesUpdate' && event.data?.settings) {
+    updateSettings(event.data?.settings)
+  }
+})
